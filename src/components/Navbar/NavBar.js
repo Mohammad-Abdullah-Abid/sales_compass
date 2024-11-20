5cc8436ca4c4f55d1ee22aec3f19eaa14ae2ecc1
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2024-11-21 00:50:57 +0500

    fix responsiveness in navbar

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -5,81 +5,79 @@ import logo from "../../images/logo.png";
 
 const NavBar = () => {
   const [top, setTop] = useState(true);
-  const [isOpen, setisOpen] = React.useState(false);
-  function handleClick() {
+  const [isOpen, setisOpen] = useState(false);
+
+  const handleClick = () => {
     setisOpen(!isOpen);
-  }
+  };
 
   useEffect(() => {
     const scrollHandler = () => {
-      window.pageYOffset > 10 ? setTop(false) : setTop(true);
+      setTop(window.pageYOffset <= 10);
     };
     window.addEventListener("scroll", scrollHandler);
     return () => window.removeEventListener("scroll", scrollHandler);
-  }, [top]);
+  }, []);
 
   return (
     <nav
-      className={`fixed top-0 w-full z-30 transition duration-300 ease-in-out mb-16 ${!top && "bg-white shadow-lg"
+      className={`fixed top-0 w-full z-30 transition duration-300 ease-in-out ${!top && "bg-white shadow-lg"
         }`}
     >
-      <div className="flex flex-row items-center justify-between py-2">
+      <div className="flex items-center justify-between px-4 py-2 md:px-8">
         {/* Logo Section */}
-        <div className="flex flex-row items-center justify-center font-semibold text-center md:px-12 md:mx-12">
-          <Link to="/" className="flex items-center space-x-3">
-            {/* Logo with Circular Border */}
-            <div className="flex items-center justify-center border-4 border-blue-900 rounded-full">
-              <img
-                src={logo}// Update the path to your logo image
-                alt="Sales Compass Logo"
-                className="w-20 h-20 rounded-full" // Ensure the logo is round
+        <Link to="/" className="flex items-center space-x-3">
+          <div className="flex items-center justify-center border-4 border-blue-900 rounded-full">
+            <img
+              src={logo}
+              alt="Sales Compass Logo"
+              className="w-10 h-10 rounded-full sm:w-12 sm:h-12 md:w-16 md:h-16"
+            />
+
+          </div>
+          <h1 className="text-2xl font-extrabold text-blue-900 md:text-4xl">
+            Sales Compass
+          </h1>
+        </Link>
+
+        {/* Hamburger Menu Button */}
+        <button
+          className="p-2 text-blue-900 rounded-md lg:hidden"
+          onClick={handleClick}
+        >
+          <svg
+            className="w-6 h-6 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            {isOpen ? (
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
               />
-            </div>
-            {/* Site Title */}
-            <h1 className="text-4xl font-extrabold text-blue-900">
-              Sales Compass
-            </h1>
-          </Link>
-        </div>
+            ) : (
+              <path
+                fillRule="evenodd"
+                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2z"
+              />
+            )}
+          </svg>
+        </button>
 
         {/* Navigation Links */}
-        <div className="flex flex-col items-center group">
-          <button
-            className="p-2 text-blue-900 rounded-lg lg:hidden"
-            onClick={handleClick}
-          >
-            <svg
-              className="w-6 h-6 fill-current"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              {isOpen && (
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-                />
-              )}
-              {!isOpen && (
-                <path
-                  fillRule="evenodd"
-                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2z"
-                />
-              )}
-            </svg>
-          </button>
-          <div className="hidden p-5 space-x-6 lg:inline-block">
-            <NavLinks />
-          </div>
+        <div className="hidden space-x-6 lg:flex">
+          <NavLinks />
+        </div>
+      </div>
 
-          <div
-            className={`fixed transition-transform duration-300 ease-in-out transit flex justify-center left-0 w-full h-auto rounded-md p-24 bg-white rounded-lg block lg:hidden shadow-xl top-14 ${isOpen ? "block" : "hidden"
-              } `}
-          >
-            <div className="flex flex-col space-y-6">
-              <NavLinks />
-            </div>
-          </div>
+      {/* Mobile Menu */}
+      <div
+        className={`absolute left-0 top-16 w-full bg-white transition-transform duration-300 ease-in-out lg:hidden ${isOpen ? "block" : "hidden"
+          }`}
+      >
+        <div className="flex flex-col items-center space-y-4 p-6">
+          <NavLinks />
         </div>
       </div>
     </nav>
